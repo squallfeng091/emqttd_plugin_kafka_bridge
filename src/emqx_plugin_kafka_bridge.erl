1d001c1d2381c6df4e9175546883c547bdf099de
fengyang <fengyang8173@boe.com.cn>
2021-02-14 21:38:07 +0800

    修改配置

--- src/emqx_plugin_kafka_bridge.erl
+++ src/emqx_plugin_kafka_bridge.erl
@@ -326,7 +326,7 @@ ekaf_get_topic() ->
 produce_kafka_payload(Message) ->
   Topic = ekaf_get_topic(),
 
-  io:format("squallfeng test :~w~n",[emqx_json:safe_encode(Message)]),
+  io:format("squallfeng test :~w~n",[Message]),
 %%  io:format("~w~n",[jiffy:decode(Message)]),
 %%  {ok, MessageBody} = emqx_json:safe_encode(Message),
 %%   MessageBody64 = base64:encode_to_string(MessageBody),
